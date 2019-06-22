7728e952d6fff3cee353807c43cb25d254fdf62a
unknown <victor.h.s.reis@gmail.com>
2019-06-22 10:55:52 -0300

    Erro na renderização do triangulo

--- OpenGlJanelas/OpenGlJanelas.cpp
+++ OpenGlJanelas/OpenGlJanelas.cpp
@@ -1,20 +1,68 @@
 // OpenGlJanelas.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
 //
-
+		
+#include <GL\glut.h>
+#include <GL/glew.h>
+#include <GL/freeglut.h>
 #include <iostream>
 
-int main()
+void display(void);
+void reshape(int w, int h);
+void mouse(int button, int state, int x, int y);
+void spinDisplay(void);
+
+
+static GLfloat spin = 0.0;
+int main(int argc, char** argv)
 {
-    std::cout << "Hello World!\n";
+	glutInit(&argc,argv);
+	glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGB);
+
+	glutInitWindowSize(250, 250);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow(argv[0]);
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+	glShadeModel(GL_FLAT);
+	glutDisplayFunc(display);
+	glutReshapeFunc(reshape);
+	glutMouseFunc(mouse);
+	glutMainLoop();
+	return 0;
 }
 
-// Executar programa: Ctrl + F5 ou Menu Depurar > Iniciar Sem Depuração
-// Depurar programa: F5 ou menu Depurar > Iniciar Depuração
+void display(void) {
+	glClear(GL_COLOR_BUFFER_BIT);
+	glPushMatrix();
+	glRotatef(spin, 0.0, 0.0, 1.0);
+	glColor3f(0.25, 0.90, 1.0);
+	glRectf(-25.0, -25.0, 25.0, 25.0);
+	glPopMatrix();
+	glutSwapBuffers();
+}
+void reshape(int w, int h) {
+	glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(-50.0, 50.0, -50.0, 50.0, -1.0, 1.0);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+}
+void mouse(int button, int state, int x, int y) {
+	switch (button) {
+	case GLUT_LEFT_BUTTON:
+		if (state == GLUT_DOWN) { glutIdleFunc(spinDisplay); }
+		break;
+	case GLUT_RIGHT_BUTTON:
+		if (state == GLUT_DOWN) { glutIdleFunc(NULL); }
+		break;
+	default:
+		break;
+	
+	}
 
-// Dicas para Começar: 
-//   1. Use a janela do Gerenciador de Soluções para adicionar/gerenciar arquivos
-//   2. Use a janela do Team Explorer para conectar-se ao controle do código-fonte
-//   3. Use a janela de Saída para ver mensagens de saída do build e outras mensagens
-//   4. Use a janela Lista de Erros para exibir erros
-//   5. Ir Para o Projeto > Adicionar Novo Item para criar novos arquivos de código, ou Projeto > Adicionar Item Existente para adicionar arquivos de código existentes ao projeto
-//   6. No futuro, para abrir este projeto novamente, vá para Arquivo > Abrir > Projeto e selecione o arquivo. sln
+}
+void spinDisplay(void) {
+	spin = spin + 2.0;
+	if (spin > 360.0) { spin = spin - 360.0; }
+	glutPostRedisplay();
+}
