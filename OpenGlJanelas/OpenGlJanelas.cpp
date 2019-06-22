615d8e45ac426307936717111d3af9ec7936e7a6
unknown <victor.h.s.reis@gmail.com>
2019-06-22 11:00:38 -0300

    erro na lib glew.h

--- OpenGlJanelas/OpenGlJanelas.cpp
+++ OpenGlJanelas/OpenGlJanelas.cpp
@@ -2,7 +2,7 @@
 //
 		
 #include <GL\glut.h>
-#include <GL/glew.h>
+//#include <GL/glew.h>
 #include <GL/freeglut.h>
 #include <iostream>
 
@@ -33,36 +33,39 @@ int main(int argc, char** argv)
 void display(void) {
 	glClear(GL_COLOR_BUFFER_BIT);
 	glPushMatrix();
-	glRotatef(spin, 0.0, 0.0, 1.0);
-	glColor3f(0.25, 0.90, 1.0);
+	glRotatef(spin, 0.0, 0.0, 1.0); //rotação
+	glColor3f(1.0, 1.0, 1.0); // branca
 	glRectf(-25.0, -25.0, 25.0, 25.0);
 	glPopMatrix();
-	glutSwapBuffers();
+	glutSwapBuffers();
 }
 void reshape(int w, int h) {
 	glViewport(0, 0, (GLsizei)w, (GLsizei)h);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	glOrtho(-50.0, 50.0, -50.0, 50.0, -1.0, 1.0);
+	glOrtho(-50.0, 50.0, -50.0, 50.0, -1.0, 1.0);
 	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
+	glLoadIdentity();
 }
 void mouse(int button, int state, int x, int y) {
 	switch (button) {
 	case GLUT_LEFT_BUTTON:
-		if (state == GLUT_DOWN) { glutIdleFunc(spinDisplay); }
+		if (state == GLUT_DOWN)
+			glutIdleFunc(spinDisplay);
 		break;
 	case GLUT_RIGHT_BUTTON:
-		if (state == GLUT_DOWN) { glutIdleFunc(NULL); }
+		if (state == GLUT_DOWN)
+			glutIdleFunc(NULL);
 		break;
 	default:
-		break;
+		break;
 	
 	}
 
 }
 void spinDisplay(void) {
 	spin = spin + 2.0;
-	if (spin > 360.0) { spin = spin - 360.0; }
-	glutPostRedisplay();
+	if (spin > 360.0)
+		spin = spin - 360.0;
+	glutPostRedisplay();
 }
